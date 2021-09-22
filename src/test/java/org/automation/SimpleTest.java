41dd4e97f74e286254aa093cdd9e879978879b62
Emilio <emilio2hd@gmail.com>
2021-09-22 01:03:45 +0100

    Initial commit

---
+++ src/test/java/org/automation/SimpleTest.java
@@ -0,0 +1,13 @@
+package org.automation;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}
