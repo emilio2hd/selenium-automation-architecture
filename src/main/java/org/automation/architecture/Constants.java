6ba85b97c90bb9af05681982657f610374e29ab5
Emílio <emilio2hd@gmail.com>
2022-09-04 22:46:48 +0100

    Add default window size and maximize as configurable properties

--- src/main/java/org/automation/architecture/Constants.java
+++ src/main/java/org/automation/architecture/Constants.java
@@ -4,7 +4,13 @@ public class Constants {
     public static final String BEAN_SIMPLE_THREAD_SCOPE = "thread";
     public static final String WEB_DRIVER_BEAN_NAME = "webDriver";
 
-    // System property key
+    /*************** System property key *********************/
+
+    // Browser Options
     public static final String BROWSER_OPTION_SYS_PROPERTY_KEY = "browserOption";
     public static final String BROWSER_HEADLESS_SYS_PROPERTY_KEY = "browserHeadless";
+
+    // Windows Size
+    public static final String WINDOW_WIDTH_PROPERTY_KEY = "windowWidth";
+    public static final String WINDOW_HEIGHT_PROPERTY_KEY = "windowHeight";
 }
