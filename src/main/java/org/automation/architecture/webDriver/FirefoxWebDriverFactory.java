60cbae493709b56fd9003ed981bdfc34f2a04b43
Emílio <emilio2hd@gmail.com>
2022-09-04 20:27:27 +0100

    Refactoring webdriver factories adding extra options

--- src/main/java/org/automation/architecture/webDriver/FirefoxWebDriverFactory.java
+++ src/main/java/org/automation/architecture/webDriver/FirefoxWebDriverFactory.java
@@ -5,23 +5,34 @@ import org.automation.architecture.TestProperties;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FirefoxWebDriverFactory {
     private static final Logger LOGGER = LoggerFactory.getLogger(FirefoxWebDriverFactory.class);
 
-    public static WebDriver create(TestProperties testProperties) {
+    public static WebDriver create(final TestProperties testProperties) {
         LOGGER.debug("Using WebDriverManager to setup firefox driver");
 
         WebDriverManager.firefoxdriver().setup();
 
+        return new FirefoxDriver(getDriverOptions(testProperties.isBrowserHeadless()));
+    }
+
+    private static FirefoxOptions getDriverOptions(final boolean isHeadless) {
         FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.addArguments("--start-maximized");
+        firefoxOptions.addArguments("--disable-web-security");
+        firefoxOptions.addArguments("--allow-running-insecure-content");
 
-        LOGGER.debug("Setting WebDriver {} ", testProperties.isBrowserHeadless() ? "headless" : "to display browser");
-        firefoxOptions.setHeadless(testProperties.isBrowserHeadless());
+        final FirefoxProfile profile = new FirefoxProfile();
+        profile.setAcceptUntrustedCertificates(true);
+        profile.setAssumeUntrustedCertificateIssuer(false);
+        firefoxOptions.setProfile(profile);
 
-        return new FirefoxDriver(firefoxOptions);
+        LOGGER.debug("Setting WebDriver {} ", isHeadless ? "headless" : "to display browser");
+        firefoxOptions.setHeadless(isHeadless);
+
+        return firefoxOptions;
     }
 }
